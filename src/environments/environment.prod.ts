7ffbe8c344caaf42c6cade7ec52ac332b6eac6eb
IkkoGa <ikkoharrison@gmail.com>
2020-05-05 16:43:13 -0400

    inital boostrap, firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyAAgXPGMXG-Pk6SdE6SHqq-fzh9iXonB_Q",
+    authDomain: "all-health.firebaseapp.com",
+    databaseURL: "https://all-health.firebaseio.com",
+    projectId: "all-health",
+    storageBucket: "all-health.appspot.com",
+    messagingSenderId: "482120187791",
+    appId: "1:482120187791:web:604737dc814668134255fa"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyAAgXPGMXG-Pk6SdE6SHqq-fzh9iXonB_Q",
+    authDomain: "all-health.firebaseapp.com",
+    databaseURL: "https://all-health.firebaseio.com",
+    projectId: "all-health",
+    storageBucket: "all-health.appspot.com",
+    messagingSenderId: "482120187791",
+    appId: "1:482120187791:web:604737dc814668134255fa"
+  }
 };
 
 /*
